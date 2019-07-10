288229f4cee1a9181e0b37719b762242ab9b0f8b
Brian VanBuren <brian@accelm.com>
2019-07-10 12:15:33 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# yii2-firebase
+
+Helper classes for communicating with Firebase servers for sending notificaion messages
---
+++ src/messaging/Messaging.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace bvb\firebase\messaging;
+
+use bvb\singleton\Singleton;
+use Kreait\Firebase;
+use Kreait\Firebase\Messaging\CloudMessage;
+use Yii;
+use yii\base\BaseObject;
+use yii\base\InvalidConfigException;
+
+/**
+ * Messaging is a helper class that
+ */
+class Messaging extends BaseObject
+{
+	/**
+	 * Implement the Singleton functionality using Yii's container
+	 */
+	use Singleton;
+
+	/**
+	 * @var string The URL of the Firebase API we are communicating with
+	 */
+	private $apiUrl = 'https://fcm.googleapis.com/fcm/send';
+
+	/**
+	 * @var string The key required to authenticate with Firebase servers
+	 */
+	public $apiKey;
+
+	/**
+	 * @var \Kreait\Firebase\Messaging
+	 */
+	public $messaging;
+
+	/**
+	 * If an API key was not supplied during insantiation it will apply one form a yii param
+	 * @throws InvalidConfigException Will throw this if an application param is not set under key ['firebase']['apiKey']
+	 * {@inheritdoc}
+	 */
+	public function init()
+	{
+		if(empty($this->apiKey) && isset(Yii::$app->params['firebase']['apiKey'])){
+			$this->apiKey = Yii::$app->params['firebase']['apiKey'];
+		} else {
+			throw new InvalidConfigException("An API key must be supplied during object creation or an applicaiton parameter must be set under the key ['firebase']['apiKey']");
+		}
+
+		$this->messaging = (new Firebase\Factory())->create()->getMessaging();
+	}
+
+	/**
+	 * Send a notification to devices
+	 * @param array $tokens
+	 * @param array $messageConfig
+	 * @return mixed
+	 */
+	public function sendNotification($tokens, $messageConfig)
+	{
+		$message = CloudMessage::fromArray([
+			'notification' => $messageConfig['notification'],
+			'data' => $messageConfig['data']
+		]);
+		$sendReport = $this->messaging->sendMulticast($message, $tokens);
+		Yii::info(print_r($sendReport,true));
+	}
+}
