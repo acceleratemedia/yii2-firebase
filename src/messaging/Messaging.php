7c0f8213cc4d8f9f605caf77c4de8306ca486827
Brian VanBuren <brian@accelm.com>
2019-07-10 16:17:44 -0400

    add serviceaccount and authorization method

--- src/messaging/Messaging.php
+++ src/messaging/Messaging.php
@@ -3,7 +3,8 @@
 namespace bvb\firebase\messaging;
 
 use bvb\singleton\Singleton;
-use Kreait\Firebase;
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\ServiceAccount;
 use Kreait\Firebase\Messaging\CloudMessage;
 use Yii;
 use yii\base\BaseObject;
@@ -30,6 +31,11 @@ class Messaging extends BaseObject
 	public $apiKey;
 
 	/**
+	 * @var string The path to the service account credentisls
+	 */
+	public $serviceAccountCredentialsPath;
+
+	/**
 	 * @var \Kreait\Firebase\Messaging
 	 */
 	public $messaging;
@@ -47,7 +53,17 @@ class Messaging extends BaseObject
 			throw new InvalidConfigException("An API key must be supplied during object creation or an applicaiton parameter must be set under the key ['firebase']['apiKey']");
 		}
 
-		$this->messaging = (new Firebase\Factory())->create()->getMessaging();
+		if(empty($this->serviceAccountCredentialsPath) && isset(Yii::$app->params['firebase']['serviceAccountCredentialsPath'])){
+			$this->serviceAccountCredentialsPath = Yii::$app->params['firebase']['serviceAccountCredentialsPath'];
+		} else {
+			throw new InvalidConfigException("An service account credentials pathmust be supplied during object creation or an applicaiton parameter must be set under the key ['firebase']['serviceAccountCredentialsPath']");
+		}
+
+		$serviceAccount = ServiceAccount::fromJsonFile(Yii::getAlias($this->serviceAccountCredentialsPath));
+		$this->messaging = (new Factory())
+			->withServiceAccount($serviceAccount)
+			->create()
+			->getMessaging();
 	}
 
 	/**
